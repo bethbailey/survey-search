607605e42407c6813fd251ac3a8fcf3066536c01
Rae <liruixue@uchicago.edu>
2018-03-03 01:46:31 -0600

    modified template

--- Django Builds/0.06/django_beta/surveysearch/surveysearch/settings.py
+++ Django Builds/0.06/django_beta/surveysearch/surveysearch/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'surveysearch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/static/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
