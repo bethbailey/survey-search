9f16571485bea5efc05bdfd1627ff1365d146290
LeosonH <leosonhoay@gmail.com>
2018-03-12 15:36:13 -0600

    commented on urls.py

--- Final Site/surveysearch/search/urls.py
+++ Final Site/surveysearch/search/urls.py
@@ -1,9 +1,10 @@
-# Code Ownership: "Direct copy"  
+# Code Ownership: "Original"  
 
 from django.urls import path
 
 from . import views
 
+# namespace
 app_name = 'search'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
@@ -11,11 +12,14 @@ urlpatterns = [
     	name='results_detail'),
     path('search_results_survey.html', views.SurveyResultsView.as_view(), \
     	name='survey_results_detail'),
+    # index objects based on primary key
     path('<int:pk>/', views.DetailView.as_view(), name='question_detail'),
     path('upload.html', views.model_form_upload, name='upload'),
     path('upload_success.html', views.upload_success, name='upload_success'),
     path('upload_failure.html', views.upload_failure, name='upload_failure'),
     path('browse_surveys.html', views.Browse.as_view(), name='browse_surveys'),
+    # index objects based on primary key
     path('browse_surveys/<int:pk>/', views.BrowseDetailView.as_view(), name='browse_detail'),
+    # index objects based on primary key
     path('question_detail/<int:pk>/', views.QuestionDetail.as_view(), name='question_detail_list')
 ]
